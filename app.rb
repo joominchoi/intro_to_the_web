4b794d0089ea4b640ff8bd8c2bdbdbfd52b1930d
JooMin Choi <joominchoi23@gmail.com>
2021-06-07 16:25:29 +0100

    added photo of cat

--- app.rb
+++ app.rb
@@ -9,4 +9,11 @@ get '/secret' do
   "don't tell anyone!"
 end
 
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+   <img src='https://i.imgur.com/jFaSxym.png'>
+  </div>"
+end
+
+
 #http://localhost:4567
