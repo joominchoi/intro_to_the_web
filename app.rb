d1d2f9ed79e32e52483c1d189a2e6ebab7593f90
JooMin Choi <joominchoi23@gmail.com>
2021-06-08 15:34:42 +0100

    introducing params

--- app.rb
+++ app.rb
@@ -11,8 +11,14 @@ get '/secret' do
   "don't tell anyone!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
 
